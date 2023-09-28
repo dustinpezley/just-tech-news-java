2273791df9270486968cdca8787c2406eb36fe22
dustinpezley <dustinpezley@gmail.com>
2023-09-28 10:33:36 -0500

    updated dependencies needed for Java 17

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -29,7 +29,7 @@ public class Post implements Serializable {
     private Date postedAt = new Date();
     @NotNull
     @Temporal(TemporalType.DATE)
-    @Column(name = "posted_at")
+    @Column(name = "updated_at")
     private Date updatedAt = new Date();
 
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
